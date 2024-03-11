3d4055312b34f066400e6f1eea4750a75bd0dd87
GenshinmasterJinHang <161272990+GenshinmasterJinHang@users.noreply.github.com>
2024-03-11 17:23:34 +0800

    Add files via upload

---
+++ demo/src/main/java/com/example/lombokdemo/Main.java
@@ -0,0 +1,12 @@
+package com.example.lombokdemo;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+      
+        Event event = new Event();
+        event.OrganizeEvent();
+        System.out.println(event);
+        System.out.println("Total number of attendees: " + event.getEventAttendees().size());
+    }}
